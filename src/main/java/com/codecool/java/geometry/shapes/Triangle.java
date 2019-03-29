cde9ecd44600ab017746f4d16e0da8dbc5f8a86b
Krzysiek <krzysiekczaja@gmail.com>
2019-03-29 11:15:52 +0100

    Add Triangle class constructor

--- src/main/java/com/codecool/java/geometry/shapes/Triangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Triangle.java
@@ -1,4 +1,35 @@
 package com.codecool.java.geometry.shapes;
 
 public class Triangle extends Shape {
+    private float a;
+    private float b;
+    private float c;
+
+    public Triangle(float a, float b, float c) {
+        if (Shape.checkIfArgsGreaterThanZero(a, b, c)) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+    @Override
+	public double calculateArea() {
+		return 0;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		return 0;
+	}
+
+	@Override
+	public String getAreaFormula() {
+		return null;
+	}
+
+	@Override
+	public String getPerimeterFormula() {
+		return null;
+	}
+    
 }
