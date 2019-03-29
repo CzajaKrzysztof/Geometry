e29bcd5082391cf36a73ee64a1434f79aac58832
Krzysiek <krzysiekczaja@gmail.com>
2019-03-29 11:30:46 +0100

    Add constructor and methods to show and calculate area and perimeter of rectangle

--- src/main/java/com/codecool/java/geometry/shapes/Rectangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Rectangle.java
@@ -1,4 +1,33 @@
 package com.codecool.java.geometry.shapes;
 
 public class Rectangle extends Shape {
+    private float a;
+    private float b;
+
+    public Rectangle(float a, float b) {
+        if (Shape.checkIfArgsGreaterThanZero(a, b)) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    @Override
+    public double calculateArea() {
+        return a * b;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * a + 2 * b;
+    }
+
+    @Override
+    public String getAreaFormula() {
+        return "a×b";
+    }
+
+    @Override
+    public String getPerimeterFormula() {
+        return "2a + 2b";
+    }
 }
