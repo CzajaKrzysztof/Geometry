5568e14a7cda65b9241dc79249ed742b8869fcdb
Krzysiek <krzysiekczaja@gmail.com>
2019-03-29 10:50:03 +0100

    Add Circle constructor

--- src/main/java/com/codecool/java/geometry/shapes/Circle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Circle.java
@@ -1,4 +1,30 @@
 package com.codecool.java.geometry.shapes;
 
 public class Circle extends Shape {
+    private float radius;
+
+    public Circle(float radius) {
+        if (Shape.checkIfArgsGreaterThanZero(radius)) {
+            this.radius = radius;
+        }
+    }
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public String getAreaFormula() {
+        return null;
+    }
+
+    @Override
+    public String getPerimeterFormula() {
+        return null;
+    }
 }
